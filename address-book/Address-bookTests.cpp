b4e3f22c23c7068e6e1dd66e282c026ba5791afd
1043882 <1043882@students.wits.ac.za>
2017-10-23 13:13:18 +0200

    Exercise 4.2

--- address-book/Address-bookTests.cpp
+++ address-book/Address-bookTests.cpp
@@ -12,7 +12,24 @@ TEST_CASE("AddressBookEntry object does not share any state with copy"){
 	AddressBookEntry entry{person, image_filename};
 	AddressBookEntry entry_copy{entry};
 	
-	CHECK(entry.getImageName() == entry_copy.getImageName());
 	entry.setImageName("newFace");
-	CHECK(entry.getImageName() == entry_copy.getImageName());
+	CHECK_FALSE(entry.getImageName() == entry_copy.getImageName());
+	
+	//The image pointers in entry and entry_copy share the same Image address
+	//Changing one changes the other
+}
+
+TEST_CASE("Assignment of AddressBookEntry object does not share any state "){
+	int age = 21;
+	string name = "Gift";
+	Person person{name,age};
+	string image_filename = "Face";
+	AddressBookEntry entry{person, image_filename};
+	AddressBookEntry entry_copy = entry;
+	
+	entry.setImageName("newFace");
+	CHECK_FALSE(entry.getImageName() == entry_copy.getImageName());
+	
+	//The image pointers in entry and entry_copy share the same Image address
+	//Changing one changes the other
 }
